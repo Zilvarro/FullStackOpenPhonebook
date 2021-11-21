1b8f854ac69120d55d8593cb81b63a70c1770c90
Zilvarro <44177726+Zilvarro@users.noreply.github.com>
2021-11-21 17:32:01 +0100

    Setup for Heroku

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # FullStackOpenPhonebook
  Phonebook Exercise from Full Stack Open 2021 course
+
+ Heroku App: https://phonebook-app-fso2021.herokuapp.com/
--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const morgan = require('morgan')
 morgan.token('body', (req, res) => JSON.stringify(req.body) )
 
+const cors = require('cors')
+
 const express = require('express')
 const app = express()
+app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
@@ -84,7 +87,7 @@ app.post('/api/persons', (request, response) => {
     response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
